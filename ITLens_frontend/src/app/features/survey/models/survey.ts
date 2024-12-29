b80e25f5ba1ea7dfb2891eab9a76860b13287f93
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-29 20:53:27 +0100

    feat: added survey and owner model

--- ITLens_frontend/src/app/features/survey/models/survey.ts
+++ ITLens_frontend/src/app/features/survey/models/survey.ts
@@ -1,2 +1,7 @@
 export interface Survey {
-}
+    id: number;
+    title: string;
+    description: string;
+    // owner: Owner;
+    // surveyEditions: SurveyEdition[];
+  }
--- ITLens_frontend/src/app/features/survey/services/survey.service.ts
+++ ITLens_frontend/src/app/features/survey/services/survey.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Survey } from '../models/survey';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SurveyService {
 
-  constructor() { }
+  private apiUrl = 'https://api.example.com/surveys'; 
+
+  constructor(private http: HttpClient) { }
+
+  getSurveys(): Observable<Survey[]> {
+    return this.http.get<Survey[]>(this.apiUrl);
+  }
 }
