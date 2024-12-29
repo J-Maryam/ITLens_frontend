0ed75a692afe30c27eeedf3c1c991ec6eb3853cd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-29 21:01:28 +0100

    refactor: added owner and survey_edition in survey model

--- ITLens_frontend/src/app/features/models/survey.ts
+++ ITLens_frontend/src/app/features/models/survey.ts
@@ -1,7 +1,10 @@
+import {Owner} from './owner';
+import {SurveyEdition} from './survey-edition';
+
 export interface Survey {
     id: number;
     title: string;
     description: string;
-    // owner: Owner;
-    // surveyEditions: SurveyEdition[];
+    owner: Owner;
+    surveyEditions: SurveyEdition[];
   }
